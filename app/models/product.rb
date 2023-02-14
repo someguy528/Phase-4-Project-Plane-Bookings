7400eeeb8eff84a9b12969385ddbe7c7975cd460
Brandon Castro <castroyou999@gmail.com>
2023-02-14 15:22:24 -0500

    changed product seed attribute

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,5 @@ class Product < ApplicationRecord
     validates :seller_id, :name, :description, :price, presence: true
     validates :name, length: {in: 4..30}, format: {with: /\A[a-zA-Z\d]+([:]{0,1}[ \-]{1}[a-zA-Z\d]+)*\z/}
     validates :description, length: {in: 5..50}, format: {with: /\A[a-zA-Z\d]+(([:\,]{0,1}[ \-]{1}[a-zA-Z\d]+)*[.!\?]{0,1})*\z/}
-    validates :price,  numericality: { greater_than: 0.00, less_than: 200.00 }
-    # , format: {with: /\A[\d]+(([\.]{1}[\d]{1,2})|([\.]{0}))\z/}
+    validates :price,  numericality: { greater_than: 0.00, less_than: 200.00 }, format: {with: /\A[\d]+(([\.]{1}[\d]{1,2})|([\.]{0}))\z/}
 end
